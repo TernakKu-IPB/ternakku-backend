a2db0daf88129864de06d7240f12ac65a60ed218
Ahmad Subhan <asdcode123@gmail.com>
2026-06-30 06:38:56 +0700

    refactor: use count instead of find unique on common service

--- src/common/animal-type.service.ts
+++ src/common/animal-type.service.ts
@@ -6,10 +6,10 @@ export class AnimalTypeService {
   constructor(private prisma: PrismaService) {}
 
   async checkAvailability(id: number, farmId: number): Promise<void> {
-    const animalType = await this.prisma.animalType.findUnique({
+    const animalType = await this.prisma.animalType.count({
       where: { id, farmId },
     });
-    if (!animalType)
+    if (animalType === 0)
       throw new NotFoundException(
         'Jenis hewan tidak ditemukan dalam peternakan Anda',
       );
--- src/common/condition-type.service.ts
+++ src/common/condition-type.service.ts
@@ -6,11 +6,10 @@ export class ConditionTypeService {
   constructor(private prisma: PrismaService) {}
 
   async checkAvailability(id: number, farmId: number): Promise<void> {
-    const conditionType = await this.prisma.conditionType.findUnique({
+    const conditionType = await this.prisma.conditionType.count({
       where: { id, farmId },
-      select: { id: true },
     });
-    if (!conditionType)
+    if (conditionType === 0)
       throw new NotFoundException(
         'Jenis kondisi tidak ditemukan dalam peternakan Anda',
       );
