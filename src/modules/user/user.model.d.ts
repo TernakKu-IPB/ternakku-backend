b0f37503e2dcacba515891358c0b5abde2d7f417
Ahmad Subhan <asdcode123@gmail.com>
2026-06-30 10:56:24 +0700

    feat(user): add user role

--- src/modules/user/user.model.d.ts
+++ src/modules/user/user.model.d.ts
@@ -1,9 +1,10 @@
-import { Gender } from '../../generated/prisma/enums';
+import { Gender, Role } from '../../generated/prisma/enums';
 
 export type User = {
   id: number;
   username: string;
   email: string;
+  role: Role;
   fullName: string;
   picture: string | null;
   birthDate: string | null;
