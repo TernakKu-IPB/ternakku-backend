fd3e4f79e42f8d0f03a166902ce98efb7314c752
Ahmad Subhan <asdcode123@gmail.com>
2026-07-21 16:08:10 +0700

    feat: use back redirect schema for reset password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -280,11 +280,7 @@ export class AuthService {
       'CLIENT_URL',
       'http://localhost:5173',
     );
-    const environment = this.config.get<'production' | 'development'>(
-      'NODE_ENV',
-      'development',
-    );
-    const resetPasswordLink = `${clientUrl}/auth/${environment === 'production' ? 'forgot-password' : 'redirect-app'}?token=${token}`;
+    const resetPasswordLink = `${clientUrl}/auth/redirect-app?token=${token}`;
 
     await this.mail.sendEmail({
       to: user.email,
